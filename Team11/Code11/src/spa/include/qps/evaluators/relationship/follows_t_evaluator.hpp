2e388c5020203ec09f0aa42c930bb4050fb661ed
timiditi <timothy.ng.jw@gmail.com>
2024-02-22 14:46:57 +0800

    refactor: add abstraction to followsT evaluator

--- Team11/Code11/src/spa/include/qps/evaluators/relationship/follows_t_evaluator.hpp
+++ Team11/Code11/src/spa/include/qps/evaluators/relationship/follows_t_evaluator.hpp
@@ -12,9 +12,45 @@ class FollowsTEvaluator : public ClauseEvaluator {
     std::shared_ptr<ReadFacade> read_facade;
     FollowsT follows_t;
 
-    static auto eval_follows_t(const std::shared_ptr<ReadFacade>& read_facade);
+    auto select_eval_method();
 
-  public:
+    // e.g. Follows*(s1, s2)
+    auto eval_follows_t(const std::shared_ptr<StmtSynonym>& stmt_syn_1,
+                        const std::shared_ptr<StmtSynonym>& stmt_syn_2) -> std::optional<Table>;
+
+    // e.g. Follows*(s1, 3)
+    auto eval_follows_t(const std::shared_ptr<StmtSynonym>& stmt_syn_1,
+                        const qps::Integer& stmt_num_2) -> std::optional<Table>;
+
+    // e.g. Follows*(s1, _)
+    auto eval_follows_t(const std::shared_ptr<StmtSynonym>& stmt_syn_1,
+                        const qps::WildCard&) -> std::optional<Table>;
+
+    // e.g. Follows*(3, s2)
+    auto eval_follows_t(const qps::Integer& stmt_num_1,
+                        const std::shared_ptr<StmtSynonym>& stmt_syn_2) -> std::optional<Table>;
+
+    // e.g. Follows*(3, 4)
+    auto eval_follows_t(const qps::Integer& stmt_num_1,
+                        const qps::Integer& stmt_num_2) -> std::optional<Table>;
+
+    // e.g. Follows*(3, _)
+    auto eval_follows_t(const qps::Integer& stmt_num_1,
+                        const qps::WildCard&) -> std::optional<Table>;
+
+    // e.g. Follows*(_, s2)
+    auto eval_follows_t(const qps::WildCard&,
+                        const std::shared_ptr<StmtSynonym>& stmt_syn_2) -> std::optional<Table>;
+
+    // e.g. Follows*(_, 3)
+    auto eval_follows_t(const qps::WildCard&,
+                        const qps::Integer& stmt_num_2) -> std::optional<Table>;
+
+    // e.g. Follows*(_, _)
+    auto eval_follows_t(const qps::WildCard&,
+                        const qps::WildCard&) -> std::optional<Table>;
+
+public:
     FollowsTEvaluator(std::shared_ptr<ReadFacade> read_facade, FollowsT follows_t)
         : ClauseEvaluator(), read_facade(std::move(read_facade)), follows_t(std::move(follows_t)) {
     }
