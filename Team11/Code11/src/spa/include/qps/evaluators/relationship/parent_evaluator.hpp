b3f5374988970f80aa138d33457a20c3c896d645
timiditi <timothy.ng.jw@gmail.com>
2024-02-23 12:04:41 +0800

    refactor: add abstraction to parent evaluator

--- Team11/Code11/src/spa/include/qps/evaluators/relationship/parent_evaluator.hpp
+++ Team11/Code11/src/spa/include/qps/evaluators/relationship/parent_evaluator.hpp
@@ -11,7 +11,43 @@ class ParentEvaluator : public ClauseEvaluator {
     std::shared_ptr<ReadFacade> read_facade;
     Parent parent;
 
-    static auto eval_parent(const std::shared_ptr<ReadFacade>& read_facade);
+    auto select_eval_method();
+
+    // e.g. Parent(s1, s2)
+    auto eval_parent(const std::shared_ptr<StmtSynonym>& stmt_syn_1,
+                     const std::shared_ptr<StmtSynonym>& stmt_syn_2) -> std::optional<Table>;
+
+    // e.g. Parent(s1, 3)
+    auto eval_parent(const std::shared_ptr<StmtSynonym>& stmt_syn_1,
+                      const qps::Integer& stmt_num_2) -> std::optional<Table>;
+
+    // e.g. Parent(s1, _)
+    auto eval_parent(const std::shared_ptr<StmtSynonym>& stmt_syn_1,
+                     const qps::WildCard&) -> std::optional<Table>;
+
+    // e.g. Parent(3, s2)
+    auto eval_parent(const qps::Integer& stmt_num_1,
+                     const std::shared_ptr<StmtSynonym>& stmt_syn_2) -> std::optional<Table>;
+
+    // e.g. Parent(3, 4)
+    auto eval_parent(const qps::Integer& stmt_num_1,
+                     const qps::Integer& stmt_num_2) -> std::optional<Table>;
+
+    // e.g. Parent(3, _)
+    auto eval_parent(const qps::Integer& stmt_num_1,
+                     const qps::WildCard&) -> std::optional<Table>;
+
+    // e.g. Parent(_, s2)
+    auto eval_parent(const qps::WildCard&,
+                     const std::shared_ptr<StmtSynonym>& stmt_syn_2) -> std::optional<Table>;
+
+    // e.g. Parent(_, 3)
+    auto eval_parent(const qps::WildCard&,
+                     const qps::Integer& stmt_num_2) -> std::optional<Table>;
+
+    // e.g. Parent(_, _)
+    auto eval_parent(const qps::WildCard&,
+                     const qps::WildCard&) -> std::optional<Table>;
 
   public:
     ParentEvaluator(std::shared_ptr<ReadFacade> read_facade, Parent parent)
