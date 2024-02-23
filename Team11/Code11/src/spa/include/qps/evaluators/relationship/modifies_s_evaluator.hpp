c3cbddf5da059b568dfc6bb65af07a9478599c33
timiditi <timothy.ng.jw@gmail.com>
2024-02-23 12:04:20 +0800

    refactor: add abstraction to modifiesS evaluator

--- Team11/Code11/src/spa/include/qps/evaluators/relationship/modifies_s_evaluator.hpp
+++ Team11/Code11/src/spa/include/qps/evaluators/relationship/modifies_s_evaluator.hpp
@@ -14,9 +14,33 @@ class ModifiesSEvaluator : public ClauseEvaluator {
     std::shared_ptr<ReadFacade> read_facade;
     ModifiesS modifies_s;
 
-    static auto eval_modifies(const std::shared_ptr<ReadFacade>& read_facade);
+    auto select_eval_method();
 
-  public:
+    // e.g. Modifies(s1, v)
+    auto eval_modifies_s(const std::shared_ptr<qps::StmtSynonym>& stmt_synonym,
+                         const std::shared_ptr<qps::VarSynonym>& var_syn) -> std::optional<Table>;
+
+    // e.g. Modifies(s1, "v")
+    auto eval_modifies_s(const std::shared_ptr<qps::StmtSynonym>& stmt_synonym,
+                         const qps::QuotedIdent& identifier) -> std::optional<Table>;
+
+    // e.g. Modifies(s1, _)
+    auto eval_modifies_s(const std::shared_ptr<qps::StmtSynonym>& stmt_synonym,
+                         const qps::WildCard&) -> std::optional<Table>;
+
+    // e.g. Modifies(3, v)
+    auto eval_modifies_s(const qps::Integer& stmt_num,
+                         const std::shared_ptr<qps::VarSynonym>& var_syn) -> std::optional<Table>;
+
+    // e.g. Modifies(3, "v")
+    auto eval_modifies_s(const qps::Integer& stmt_num,
+                         const qps::QuotedIdent& identifier) -> std::optional<Table>;
+
+    // e.g. Modifies(3, _)
+    auto eval_modifies_s(const qps::Integer& stmt_num,
+                         const qps::WildCard&) -> std::optional<Table>;
+
+public:
     ModifiesSEvaluator(std::shared_ptr<ReadFacade> read_facade, ModifiesS modifies_s)
         : ClauseEvaluator(), read_facade(std::move(read_facade)), modifies_s(std::move(modifies_s)) {
     }
