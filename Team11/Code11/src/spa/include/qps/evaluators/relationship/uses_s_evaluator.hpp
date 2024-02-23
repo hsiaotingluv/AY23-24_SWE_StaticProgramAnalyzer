78ac6eb6e95bcb3d90725bcfbdfa5a6db2143bf2
timiditi <timothy.ng.jw@gmail.com>
2024-02-23 12:05:02 +0800

    refactor: add abstraction to usesS evaluator

--- Team11/Code11/src/spa/include/qps/evaluators/relationship/uses_s_evaluator.hpp
+++ Team11/Code11/src/spa/include/qps/evaluators/relationship/uses_s_evaluator.hpp
@@ -11,7 +11,31 @@ class UsesSEvaluator : public ClauseEvaluator {
     std::shared_ptr<ReadFacade> read_facade;
     UsesS uses_s;
 
-    static auto eval_uses_s(const std::shared_ptr<ReadFacade>& read_facade);
+    auto select_eval_method();
+
+    // e.g. Uses(a/pn/s, v)
+    auto eval_uses_s(const std::shared_ptr<StmtSynonym>& stmt_synonym,
+                     const std::shared_ptr<VarSynonym>& var_synonym) -> std::optional<Table>;
+
+    // e.g. Uses(a/pn/s, "v")
+    auto eval_uses_s(const std::shared_ptr<StmtSynonym>& stmt_synonym,
+                     const qps::QuotedIdent& quoted_ident) -> std::optional<Table>;
+
+    // e.g. Uses(a/pn/s, _)
+    auto eval_uses_s(const std::shared_ptr<StmtSynonym>& stmt_synonym,
+                     const qps::WildCard&) -> std::optional<Table>;
+
+    // e.g. Uses(3, v)
+    auto eval_uses_s(const qps::Integer& stmt_num,
+                     const std::shared_ptr<VarSynonym>& var_synonym) -> std::optional<Table>;
+
+    // e.g. Uses(3, "v")
+    auto eval_uses_s(const qps::Integer& stmt_num,
+                     const qps::QuotedIdent& quoted_ident) -> std::optional<Table>;
+
+    // e.g. Uses(3, _)
+    auto eval_uses_s(const qps::Integer& stmt_num,
+                     const qps::WildCard&) -> std::optional<Table>;
 
   public:
     UsesSEvaluator(std::shared_ptr<ReadFacade> read_facade, UsesS uses_s)
