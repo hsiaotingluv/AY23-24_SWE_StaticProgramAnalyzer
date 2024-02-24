493f4ea7c10572f897645f8d0ca4c2e2929eac76
bernarduskrishna <krishnabernardus@gmail.com>
2024-02-24 19:45:48 +0800

    Remove unused class and rename abstraction to abstract_stores

--- Team11/Code11/src/spa/include/pkb/stores/parent_store/parent_star_store.h
+++ Team11/Code11/src/spa/include/pkb/stores/parent_store/parent_star_store.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "pkb/abstraction/many_to_many_store.h"
+#include "pkb/abstract_stores/many_to_many_store.h"
 #include "pkb/common_types/statement_number.h"
 
 #include <string>
