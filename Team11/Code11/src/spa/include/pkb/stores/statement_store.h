df5a5ac7b791bbf6fed1cb7270194c83c7a4d46b
Amadeus Winarto <amadeusw6@gmail.com>
2024-02-26 09:14:32 +0800

    Fix: missing header

--- Team11/Code11/src/spa/include/pkb/stores/statement_store.h
+++ Team11/Code11/src/spa/include/pkb/stores/statement_store.h
@@ -1,8 +1,7 @@
 #pragma once
 
-#include <string>
-
 #include "common/statement_type.hpp"
 #include "pkb/abstract_stores/many_to_one_store.h"
+#include "pkb/common_types/statement_number.h"
 
 class StatementStore : public ManyToOneStore<StatementNumber, StatementType> {};
