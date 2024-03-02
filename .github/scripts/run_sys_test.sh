e7c18c62a1d9db1dd4d8c9849b43c87bdba2d984
Amadeus Winarto <amadeusw6@gmail.com>
2024-03-02 12:56:48 +0800

    Skip M1 systems tests

--- .github/scripts/run_sys_test.sh
+++ .github/scripts/run_sys_test.sh
@@ -5,6 +5,11 @@ accumulated_result=""
 for source_file in $(find "Team11/Tests11" -type f -name "*_source.txt"); do
   input_file="${source_file/_source.txt/_queries.txt}"
 
+  # Skip if "Milestone1" is in the source file name
+  if [[ $source_file == *"Milestone1"* ]]; then
+    continue
+  fi
+
   output=$(python3 Team11/run_autotester.py "$source_file" "$input_file" -i)
 
   # Extract the last line of the output
