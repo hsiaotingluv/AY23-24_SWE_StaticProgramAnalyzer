96379e61c7099a7400f33214bf8b85166698457a
Amadeus Winarto <amadeusw6@gmail.com>
2024-03-04 15:11:27 +0800

    Feat: add attribute entities

---
+++ Team11/Code11/src/spa/include/qps/parser/entities/attribute_name.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "qps/template_utils.hpp"
+#include <array>
+#include <type_traits>
+
+namespace qps {
+
+struct ProcName {
+    static constexpr auto keyword = std::array<std::string_view, 1>{"procName"};
+};
+
+struct VarName {
+    static constexpr auto keyword = std::array<std::string_view, 1>{"varName"};
+};
+
+struct Value {
+    static constexpr auto keyword = std::array<std::string_view, 1>{"value"};
+};
+
+struct StmtNum {
+    static constexpr auto keyword = std::array<std::string_view, 2>{"stmt", "#"};
+};
+
+using AttrNameList = TypeList<ProcName, VarName, Value, StmtNum>;
+using AttrName = type_list_to_variant_t<AttrNameList>;
+
+template <typename T, std::enable_if_t<is_variant_member_v<T, AttrName>, int> = 0>
+auto operator==(const T&, const T&) -> bool {
+    return true;
+}
+
+template <typename T, typename S, std::enable_if_t<is_variant_member_v<T, AttrName>, int> = 0,
+          std::enable_if_t<is_variant_member_v<S, AttrName>, int> = 0, std::enable_if_t<!std::is_same_v<T, S>, int> = 0>
+auto operator==(const T&, const S&) -> bool {
+    return false;
+}
+
+template <unsigned long N, typename T>
+auto operator<<(std::ostream& os, const std::array<T, N>& keywords) -> std::ostream& {
+    for (const auto& keyword : keywords) {
+        os << keyword;
+    }
+    return os;
+}
+
+template <typename T, std::enable_if_t<is_variant_member_v<T, AttrName>, int> = 0>
+auto operator<<(std::ostream& os, const T&) -> std::ostream& {
+    os << T::keyword;
+    return os;
+}
+
+} // namespace qps
