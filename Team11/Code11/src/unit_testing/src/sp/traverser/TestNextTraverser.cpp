856cf089e72f89ae305c900f3149170ff16acbdb
Chan Choon Yong <43673767+choonyongchan@users.noreply.github.com>
2024-03-08 22:57:18 +0800

    Test: [CFG] Isolate test to focus on NextTraverser

--- Team11/Code11/src/unit_testing/src/sp/traverser/TestNextTraverser.cpp
+++ Team11/Code11/src/unit_testing/src/sp/traverser/TestNextTraverser.cpp
@@ -11,8 +11,15 @@
 #include "sp/traverser/traverser.hpp"
 
 TEST_CASE("Usability Test on Next Traverser") {
+    auto tokenizer_runner =std::make_shared<tokenizer::TokenizerRunner>(std::make_unique<sp::SourceProcessorTokenizer>(), true);
+    auto parser = std::make_shared<sp::ProgramParser>();
     auto [read_facade, write_facade] = PKB::create_facades();
-    auto sp = sp::SourceProcessor::get_complete_sp(write_facade);
+    auto cfg_builder = std::make_shared<sp::CfgBuilder>();
+    auto stmt_num_traverser = std::make_shared<sp::StmtNumTraverser>(write_facade);
+    std::vector<std::shared_ptr<sp::Traverser>> design_abstr_traversers = {};
+    auto next_traverser = std::make_shared<sp::NextTraverser>(write_facade);
+    auto sp = sp::SourceProcessor{tokenizer_runner,        parser,        stmt_num_traverser, cfg_builder,
+                                  design_abstr_traversers, next_traverser};
 
     SECTION("Complex Program Code - success") {
         std::string input = R"(procedure main {
@@ -53,7 +60,7 @@ TEST_CASE("Usability Test on Next Traverser") {
             normSq = cenX * cenX + cenY * cenY;
         })";
 
-        auto ast = sp->process(input);            // Usability Test. Should not throw any errors.
+        auto ast = sp.process(input);            // Usability Test. Should not throw any errors.
         REQUIRE(ast->T == sp::NodeType::Program); // This is a placeholder test.
     }
 
@@ -84,7 +91,7 @@ TEST_CASE("Usability Test on Next Traverser") {
                             read j;}}
             })";
 
-        auto ast = sp->process(input);            // Usability Test. Should not throw any errors.
+        auto ast = sp.process(input);            // Usability Test. Should not throw any errors.
         REQUIRE(ast->T == sp::NodeType::Program); // This is a placeholder test.
     }
 }
