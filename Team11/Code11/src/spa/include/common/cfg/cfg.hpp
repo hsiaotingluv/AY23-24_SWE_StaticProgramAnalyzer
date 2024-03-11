607c3d55778c80ccd119e62a31303795ae91e2ef
Simon Julian Lauw <simonjulian21@gmail.com>
2024-03-11 21:51:51 +0800

    Refactor: move cfg from hpp to cpp

--- Team11/Code11/src/spa/include/common/cfg/cfg.hpp
+++ Team11/Code11/src/spa/include/common/cfg/cfg.hpp
@@ -23,38 +23,22 @@ class CfgNode {
     /**
      * @brief Get all statement numbers in the CfgNode.
      */
-    auto get() const -> StatementNumbers {
-        return stmt_nums;
-    };
+    auto get() const -> StatementNumbers;
 
     /**
      * @brief Construct a new Cfg Node object
      */
-    auto add(int stmt_num) -> void {
-        stmt_nums.push_back(stmt_num);
-    };
+    auto add(int stmt_num) -> void;
 
     /**
      * @brief Check if the CfgNode has no statement numbers.
      */
-    auto empty() const -> bool {
-        return stmt_nums.size() == 0;
-    };
+    auto empty() const -> bool;
 
     /**
      * @brief Construct a string representation of the CfgNode. e.g. "Node(4, 5, 6)"
      */
-    friend auto operator<<(std::ostream& os, const CfgNode& cfg_node) -> std::ostream& {
-        os << "Node(";
-        for (size_t i = 0; i < cfg_node.stmt_nums.size(); i++) {
-            if (i != 0) {
-                os << ", ";
-            }
-            os << cfg_node.stmt_nums.at(i);
-        }
-        os << ")";
-        return os;
-    }
+    friend auto operator<<(std::ostream& os, const CfgNode& cfg_node) -> std::ostream&;
 };
 
 /**
@@ -88,104 +72,59 @@ class Cfg {
     /**
      * @brief Get the start node.
      */
-    auto get_start_node() const -> std::shared_ptr<CfgNode> {
-        return start_node;
-    };
+    auto get_start_node() const -> std::shared_ptr<CfgNode>;
 
     /**
      * @brief Get the current node.
      */
-    auto get_current_node() const -> std::shared_ptr<CfgNode> {
-        return current_node;
-    };
+    auto get_current_node() const -> std::shared_ptr<CfgNode>;
 
     /**
      * @brief Get the associated OutNeighbours
      */
-    auto get_outneighbours(std::shared_ptr<CfgNode> node) const -> OutNeighbours {
-        if (graph.find(node) == graph.end()) {
-            return EMPTY_OUTNEIGHBOURS;
-        } else {
-            return graph.at(node);
-        }
-    };
+    auto get_outneighbours(const std::shared_ptr<CfgNode>& node) const -> OutNeighbours;
 
     /**
      * @brief Get the graph.
      */
-    auto get_graph() const -> Graph {
-        return graph;
-    };
+    auto get_graph() const -> Graph;
 
     /**
      * @brief Check if the current node is empty.
      */
-    auto is_current_node_empty() const -> bool {
-        return current_node->empty();
-    }
+    auto is_current_node_empty() const -> bool;
 
     /**
      * @brief Add a statement number to the current node.
      */
-    auto add_stmt_to_node(int stmt_num) -> void {
-        current_node->add(stmt_num);
-    };
+    auto add_stmt_to_node(int stmt_num) -> void;
 
     /**
      * @brief Add a new node to the graph, with empty out-neighbours.
      */
-    auto add_node_to_graph() -> void {
-        auto edge = std::make_pair(current_node, EMPTY_OUTNEIGHBOURS);
-        graph.insert(edge);
-    }
+    auto add_node_to_graph() -> void;
 
     /**
      * @brief Add a new node to the graph and link it to the current node.
      */
-    auto add_outneighbour_to_graph(std::shared_ptr<CfgNode> outneighbour) -> void {
-        // Fill the out-neighbours of current_node.
-        if (graph.at(current_node).first) {
-            graph.at(current_node).second = outneighbour;
-        } else {
-            graph.at(current_node).first = outneighbour;
-        }
-    }
+    auto add_outneighbour_to_graph(const std::shared_ptr<CfgNode>& outneighbour) -> void;
 
     /**
      * @brief Move current node to the next node (Need not be a outneighbour node).
      * @note This is only explicitly used for If CfgNode to build both branches of the If statement.
      */
-    auto move_to(std::shared_ptr<CfgNode> node) -> void {
-        current_node = node;
-    };
+    auto move_to(std::shared_ptr<CfgNode> node) -> void;
 
     /**
      * @brief Add outneighbour node to the graph and move current node to the outneighbour node.
      * @note Default way to traverse the Cfg.
      */
-    auto link_and_move_to(std::shared_ptr<CfgNode> node) -> void {
-        add_outneighbour_to_graph(node);
-        move_to(node);
-        add_node_to_graph();
-    };
+    auto link_and_move_to(std::shared_ptr<CfgNode> node) -> void;
 
     /**
      * @brief Construct a string representation of the Cfg. e.g. "Node(4, 5, 6) -> OutNeighbours(Node(7, 8, 9), Node(10,
      * 11, 12))"
      */
-    friend auto operator<<(std::ostream& os, const Cfg& cfg) -> std::ostream& {
-        auto graph = cfg.graph;
-        for (const auto& [node, outneighbours] : graph) {
-            os << *node << " -> OutNeighbours(";
-            if (outneighbours.first) {
-                os << *outneighbours.first;
-            }
-            if (outneighbours.second) {
-                os << ", " << *outneighbours.second;
-            }
-            os << ")\n";
-        }
-        return os;
-    }
+    friend auto operator<<(std::ostream& os, const Cfg& cfg) -> std::ostream&;
 };
 } // namespace sp
