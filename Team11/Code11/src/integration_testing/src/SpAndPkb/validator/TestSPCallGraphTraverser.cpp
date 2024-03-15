1993cabec24c78a5f895b41ab617ef18018b7d44
Chan Choon Yong <43673767+choonyongchan@users.noreply.github.com>
2024-03-15 12:14:25 +0800

    Adhere to Linting Style

--- Team11/Code11/src/integration_testing/src/SpAndPkb/validator/TestSPCallGraphTraverser.cpp
+++ Team11/Code11/src/integration_testing/src/SpAndPkb/validator/TestSPCallGraphTraverser.cpp
@@ -1,6 +1,6 @@
 #include "catch.hpp"
-#include "sp/validator/call_graph_traverser.hpp"
 #include "pkb/facades/read_facade.h"
+#include "sp/validator/call_graph_traverser.hpp"
 
 using namespace sp;
 using ProcNames = std::unordered_set<std::string>;
@@ -17,20 +17,15 @@ auto traverse(const sp::SemanticValidator::CallGraph& call_graph) -> std::shared
 
 TEST_CASE("Test SP Call Graph Traverser - Simple Usability Tests") {
     const sp::SemanticValidator::CallGraph EMPTY_CALLGRAPH = {};
-    const sp::SemanticValidator::CallGraph ONE_TO_NONE_CALLGRAPH = {
-        {"Procedure1", ProcNames()},
-        {"Procedure2", ProcNames()}
-    };
-    const sp::SemanticValidator::CallGraph ONE_TO_ONE_CALLGRAPH = {
-        {"Procedure1", ProcNames({"Procedure2"})},
-        {"Procedure2", ProcNames({"Procedure3"})}
-    };
+    const sp::SemanticValidator::CallGraph ONE_TO_NONE_CALLGRAPH = {{"Procedure1", ProcNames()},
+                                                                    {"Procedure2", ProcNames()}};
+    const sp::SemanticValidator::CallGraph ONE_TO_ONE_CALLGRAPH = {{"Procedure1", ProcNames({"Procedure2"})},
+                                                                   {"Procedure2", ProcNames({"Procedure3"})}};
     const sp::SemanticValidator::CallGraph ONE_TO_MANY_CALLGRAPH = {
         {"Procedure1", ProcNames({"Procedure2", "Procedure3", "Procedure4"})},
         {"Procedure2", ProcNames({"Procedure3", "Procedure4", "Procedure5"})},
-        {"Procedure3", ProcNames({"Procedure4", "Procedure5", "Procedure6"})}
-    };
-    
+        {"Procedure3", ProcNames({"Procedure4", "Procedure5", "Procedure6"})}};
+
     SECTION("Test Traversing an Empty CallGraph - Success") {
         auto read_facade = traverse(EMPTY_CALLGRAPH);
         REQUIRE(read_facade->get_all_calls_callees() == ProcNames());
@@ -54,7 +49,8 @@ TEST_CASE("Test SP Call Graph Traverser - Simple Usability Tests") {
     SECTION("Test Traversing a CallGraph with Multiple Callers - Success") {
         auto read_facade = traverse(ONE_TO_MANY_CALLGRAPH);
         REQUIRE(read_facade->get_all_calls_callees() == ProcNames({"Procedure1", "Procedure2", "Procedure3"}));
-        REQUIRE(read_facade->get_all_calls_callers() == ProcNames({"Procedure2", "Procedure3", "Procedure4", "Procedure5", "Procedure6"}));
+        REQUIRE(read_facade->get_all_calls_callers() ==
+                ProcNames({"Procedure2", "Procedure3", "Procedure4", "Procedure5", "Procedure6"}));
         REQUIRE(read_facade->has_calls_relation("Procedure2", "Procedure1"));
         REQUIRE(read_facade->has_calls_relation("Procedure3", "Procedure1"));
         REQUIRE(read_facade->has_calls_relation("Procedure4", "Procedure1"));
