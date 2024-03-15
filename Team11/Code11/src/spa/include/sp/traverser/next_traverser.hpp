6631100626a37d2b993f64301c2a17162dc7346f
Amadeus Winarto <amadeusw6@gmail.com>
2024-03-15 14:06:14 +0800

    Improve const correctness

--- Team11/Code11/src/spa/include/sp/traverser/next_traverser.hpp
+++ Team11/Code11/src/spa/include/sp/traverser/next_traverser.hpp
@@ -10,13 +10,13 @@ class NextTraverser {
     std::shared_ptr<pkb::WriteFacade> write_facade;
 
   private:
-    auto traverse_node(std::shared_ptr<CfgNode> node) -> void;
-    auto traverse_edge(std::shared_ptr<CfgNode> node, std::shared_ptr<CfgNode> outneighbour) -> void;
-    auto traverse_edges(std::shared_ptr<CfgNode> node, OutNeighbours outneighbours) -> void;
-    auto traverse_procedure(std::shared_ptr<Cfg> cfg) -> void;
+    auto traverse_node(const std::shared_ptr<CfgNode>& node) -> void;
+    auto traverse_edge(const std::shared_ptr<CfgNode>& node, const std::shared_ptr<CfgNode>& outneighbour) -> void;
+    auto traverse_edges(const std::shared_ptr<CfgNode>& node, const OutNeighbours& outneighbours) -> void;
+    auto traverse_procedure(const std::shared_ptr<Cfg>& cfg) -> void;
 
   public:
     explicit NextTraverser(std::shared_ptr<pkb::WriteFacade> write_facade) : write_facade(std::move(write_facade)){};
-    auto traverse(ProcMap cfgs) -> void;
+    auto traverse(const ProcMap& cfgs) -> void;
 };
 } // namespace sp
