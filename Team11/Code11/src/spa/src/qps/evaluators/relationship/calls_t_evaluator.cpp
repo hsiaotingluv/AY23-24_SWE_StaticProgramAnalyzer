875520a9dcbf80d073fb19b4c0e46155b224ceb6
timiditi <timothy.ng.jw@gmail.com>
2024-03-19 14:04:10 +0800

    fix: remove recursion support in calls evaluation

--- Team11/Code11/src/spa/src/qps/evaluators/relationship/calls_t_evaluator.cpp
+++ Team11/Code11/src/spa/src/qps/evaluators/relationship/calls_t_evaluator.cpp
@@ -50,7 +50,11 @@ auto CallsTEvaluator::evaluate() const -> OutputTable {
 
 auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_syn_1,
                                    const std::shared_ptr<ProcSynonym>& proc_syn_2) const -> OutputTable {
-    // TODO improve pkb api with get all caller-callee pairs
+    // No recursion in SIMPLE
+    if (proc_syn_1 == proc_syn_2) {
+        return Table{};
+    }
+// TODO improve pkb api with get all caller-callee pairs
     auto table = Table{{proc_syn_1, proc_syn_2}};
     const auto all_callers = read_facade->get_all_calls_star_keys();
     for (const auto& caller : all_callers) {
