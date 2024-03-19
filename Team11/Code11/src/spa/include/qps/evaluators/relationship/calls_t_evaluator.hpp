f01d3d9fbbd6dc26fffec924629d57939f85048c
Amadeus Winarto <amadeusw6@gmail.com>
2024-03-19 10:46:57 +0800

    Chore: fix formatting

--- Team11/Code11/src/spa/include/qps/evaluators/relationship/calls_t_evaluator.hpp
+++ Team11/Code11/src/spa/include/qps/evaluators/relationship/calls_t_evaluator.hpp
@@ -3,54 +3,50 @@
 #include "clause_evaluator.hpp"
 
 namespace qps {
-    class CallsTEvaluator : public ClauseEvaluator {
-        std::shared_ptr<pkb::ReadFacade> read_facade;
-        CallsT calls_t;
+class CallsTEvaluator : public ClauseEvaluator {
+    std::shared_ptr<pkb::ReadFacade> read_facade;
+    CallsT calls_t;
 
-        [[nodiscard]] auto select_eval_method() const;
+    [[nodiscard]] auto select_eval_method() const;
 
-        // e.g. Calls*(p, q)
-        [[nodiscard]] auto eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_1,
-                                        const std::shared_ptr<ProcSynonym>& proc_2) const -> OutputTable;
+    // e.g. Calls*(p, q)
+    [[nodiscard]] auto eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_1,
+                                    const std::shared_ptr<ProcSynonym>& proc_2) const -> OutputTable;
 
-        // e.g. Calls*(p, "q")
-        [[nodiscard]] auto eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_1,
-                                        const qps::QuotedIdent& quoted_proc_2) const -> OutputTable;
+    // e.g. Calls*(p, "q")
+    [[nodiscard]] auto eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_1,
+                                    const qps::QuotedIdent& quoted_proc_2) const -> OutputTable;
 
-        // e.g. Calls*(p, _)
-        [[nodiscard]] auto eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_1,
-                                        const qps::WildCard&) const -> OutputTable;
+    // e.g. Calls*(p, _)
+    [[nodiscard]] auto eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_1, const qps::WildCard&) const
+        -> OutputTable;
 
-        // e.g. Calls*("p", q)
-        [[nodiscard]] auto eval_calls_t(const qps::QuotedIdent& quoted_proc_1,
-                                        const std::shared_ptr<ProcSynonym>& proc_2) const -> OutputTable;
+    // e.g. Calls*("p", q)
+    [[nodiscard]] auto eval_calls_t(const qps::QuotedIdent& quoted_proc_1,
+                                    const std::shared_ptr<ProcSynonym>& proc_2) const -> OutputTable;
 
-        // e.g. Calls*("p", "q")
-        [[nodiscard]] auto eval_calls_t(const qps::QuotedIdent& quoted_proc_1,
-                                        const qps::QuotedIdent& quoted_proc_2) const -> OutputTable;
+    // e.g. Calls*("p", "q")
+    [[nodiscard]] auto eval_calls_t(const qps::QuotedIdent& quoted_proc_1, const qps::QuotedIdent& quoted_proc_2) const
+        -> OutputTable;
 
-        // e.g. Calls*("p", _)
-        [[nodiscard]] auto eval_calls_t(const qps::QuotedIdent& quoted_proc_1,
-                                        const qps::WildCard&) const -> OutputTable;
+    // e.g. Calls*("p", _)
+    [[nodiscard]] auto eval_calls_t(const qps::QuotedIdent& quoted_proc_1, const qps::WildCard&) const -> OutputTable;
 
-        // e.g. Calls*(_, q)
-        [[nodiscard]] auto eval_calls_t(const qps::WildCard&,
-                                        const std::shared_ptr<ProcSynonym>& proc_2) const -> OutputTable;
+    // e.g. Calls*(_, q)
+    [[nodiscard]] auto eval_calls_t(const qps::WildCard&, const std::shared_ptr<ProcSynonym>& proc_2) const
+        -> OutputTable;
 
-        // e.g. Calls*(_, "q")
-        [[nodiscard]] auto eval_calls_t(const qps::WildCard&,
-                                        const qps::QuotedIdent& quoted_proc_2) const -> OutputTable;
+    // e.g. Calls*(_, "q")
+    [[nodiscard]] auto eval_calls_t(const qps::WildCard&, const qps::QuotedIdent& quoted_proc_2) const -> OutputTable;
 
-        // e.g. Calls*(_, _)
-        [[nodiscard]] auto eval_calls_t(const qps::WildCard&,
-                                        const qps::WildCard&) const -> OutputTable;
+    // e.g. Calls*(_, _)
+    [[nodiscard]] auto eval_calls_t(const qps::WildCard&, const qps::WildCard&) const -> OutputTable;
 
-    public:
-        CallsTEvaluator(std::shared_ptr<pkb::ReadFacade>read_facade, CallsT calls_t)
-                : ClauseEvaluator(), read_facade(std::move(read_facade)), calls_t(std::move(calls_t)) {
-        }
+  public:
+    CallsTEvaluator(std::shared_ptr<pkb::ReadFacade> read_facade, CallsT calls_t)
+        : ClauseEvaluator(), read_facade(std::move(read_facade)), calls_t(std::move(calls_t)) {
+    }
 
-        [[nodiscard]] auto evaluate() const -> OutputTable override;
-
-    };
-}
+    [[nodiscard]] auto evaluate() const -> OutputTable override;
+};
+} // namespace qps
--- Team11/Code11/src/spa/src/qps/evaluators/relationship/calls_t_evaluator.cpp
+++ Team11/Code11/src/spa/src/qps/evaluators/relationship/calls_t_evaluator.cpp
@@ -4,51 +4,43 @@ namespace qps {
 
 auto CallsTEvaluator::select_eval_method() const {
     return overloaded{
-            // e.g. Calls*(p, q)
-            [this](const std::shared_ptr<ProcSynonym> &proc_syn_1,
-                   const std::shared_ptr<ProcSynonym> &proc_syn_2) -> OutputTable {
-                return eval_calls_t(proc_syn_1, proc_syn_2);
-            },
-            // e.g. Calls*(p, "q")
-            [this](const std::shared_ptr<ProcSynonym> &proc_syn_1,
-                   const QuotedIdent &quoted_proc_2) -> OutputTable {
-                return eval_calls_t(proc_syn_1, quoted_proc_2);
-            },
-            // e.g. Calls*(p, _)
-            [this](const std::shared_ptr<ProcSynonym> &proc_syn_1,
-                   const WildCard& wild_card_2) -> OutputTable {
-                return eval_calls_t(proc_syn_1, wild_card_2);
-            },
-            // e.g. Calls*("p", q)
-            [this](const QuotedIdent &quoted_proc_1,
-                   const std::shared_ptr<ProcSynonym> &proc_syn_2) -> OutputTable {
-                return eval_calls_t(quoted_proc_1, proc_syn_2);
-            },
-            // e.g. Calls*("p", "q")
-            [this](const QuotedIdent &quoted_proc_1,
-                   const QuotedIdent &quoted_proc_2) -> OutputTable {
-                return eval_calls_t(quoted_proc_1, quoted_proc_2);
-            },
-            // e.g. Calls*("p", _)
-            [this](const QuotedIdent &quoted_proc_1,
-                   const WildCard &wild_card_2) -> OutputTable {
-                return eval_calls_t(quoted_proc_1, wild_card_2);
-            },
-            // e.g. Calls*(_, q)
-            [this](const WildCard& wild_card_1,
-                   const std::shared_ptr<ProcSynonym> &proc_syn_2) -> OutputTable {
-                return eval_calls_t(wild_card_1, proc_syn_2);
-            },
-            // e.g. Calls*(_, "q")
-            [this](const WildCard &wild_card_1,
-                   const QuotedIdent &quoted_proc_2) -> OutputTable {
-                return eval_calls_t(wild_card_1, quoted_proc_2);
-            },
-            // e.g. Calls*(_, _)
-            [this](const WildCard &wild_card_1,
-                   const WildCard &wild_card_2) -> OutputTable {
-                return eval_calls_t(wild_card_1, wild_card_2);
-            },
+        // e.g. Calls*(p, q)
+        [this](const std::shared_ptr<ProcSynonym>& proc_syn_1,
+               const std::shared_ptr<ProcSynonym>& proc_syn_2) -> OutputTable {
+            return eval_calls_t(proc_syn_1, proc_syn_2);
+        },
+        // e.g. Calls*(p, "q")
+        [this](const std::shared_ptr<ProcSynonym>& proc_syn_1, const QuotedIdent& quoted_proc_2) -> OutputTable {
+            return eval_calls_t(proc_syn_1, quoted_proc_2);
+        },
+        // e.g. Calls*(p, _)
+        [this](const std::shared_ptr<ProcSynonym>& proc_syn_1, const WildCard& wild_card_2) -> OutputTable {
+            return eval_calls_t(proc_syn_1, wild_card_2);
+        },
+        // e.g. Calls*("p", q)
+        [this](const QuotedIdent& quoted_proc_1, const std::shared_ptr<ProcSynonym>& proc_syn_2) -> OutputTable {
+            return eval_calls_t(quoted_proc_1, proc_syn_2);
+        },
+        // e.g. Calls*("p", "q")
+        [this](const QuotedIdent& quoted_proc_1, const QuotedIdent& quoted_proc_2) -> OutputTable {
+            return eval_calls_t(quoted_proc_1, quoted_proc_2);
+        },
+        // e.g. Calls*("p", _)
+        [this](const QuotedIdent& quoted_proc_1, const WildCard& wild_card_2) -> OutputTable {
+            return eval_calls_t(quoted_proc_1, wild_card_2);
+        },
+        // e.g. Calls*(_, q)
+        [this](const WildCard& wild_card_1, const std::shared_ptr<ProcSynonym>& proc_syn_2) -> OutputTable {
+            return eval_calls_t(wild_card_1, proc_syn_2);
+        },
+        // e.g. Calls*(_, "q")
+        [this](const WildCard& wild_card_1, const QuotedIdent& quoted_proc_2) -> OutputTable {
+            return eval_calls_t(wild_card_1, quoted_proc_2);
+        },
+        // e.g. Calls*(_, _)
+        [this](const WildCard& wild_card_1, const WildCard& wild_card_2) -> OutputTable {
+            return eval_calls_t(wild_card_1, wild_card_2);
+        },
     };
 }
 
@@ -56,8 +48,8 @@ auto CallsTEvaluator::evaluate() const -> OutputTable {
     return std::visit(select_eval_method(), calls_t.procedure1, calls_t.procedure2);
 }
 
-auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym> &proc_syn_1,
-                                   const std::shared_ptr<ProcSynonym> &proc_syn_2) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_syn_1,
+                                   const std::shared_ptr<ProcSynonym>& proc_syn_2) const -> OutputTable {
     // TODO improve pkb api with get all caller-callee pairs
     auto table = Table{{proc_syn_1, proc_syn_2}};
     const auto all_callers = read_facade->get_all_calls_star_keys();
@@ -70,8 +62,8 @@ auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym> &proc_syn_
     return table;
 }
 
-auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym> &proc_syn_1,
-                                   const QuotedIdent &quoted_proc_2) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_syn_1,
+                                   const QuotedIdent& quoted_proc_2) const -> OutputTable {
     auto table = Table{{proc_syn_1}};
     const auto all_callers = read_facade->get_star_callers(quoted_proc_2.get_value());
     for (const auto& caller : all_callers) {
@@ -80,8 +72,8 @@ auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym> &proc_syn_
     return table;
 }
 
-auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym> &proc_syn_1,
-                                   const WildCard &) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym>& proc_syn_1, const WildCard&) const
+    -> OutputTable {
     auto table = Table{{proc_syn_1}};
     const auto all_callers = read_facade->get_all_calls_star_keys();
     for (const auto& caller : all_callers) {
@@ -90,8 +82,8 @@ auto CallsTEvaluator::eval_calls_t(const std::shared_ptr<ProcSynonym> &proc_syn_
     return table;
 }
 
-auto CallsTEvaluator::eval_calls_t(const QuotedIdent &quoted_proc_1,
-                                   const std::shared_ptr<ProcSynonym> &proc_syn_2) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const QuotedIdent& quoted_proc_1,
+                                   const std::shared_ptr<ProcSynonym>& proc_syn_2) const -> OutputTable {
     auto table = Table{{proc_syn_2}};
     const auto all_callees = read_facade->get_star_callees(quoted_proc_1.get_value());
     for (const auto& callee : all_callees) {
@@ -100,8 +92,8 @@ auto CallsTEvaluator::eval_calls_t(const QuotedIdent &quoted_proc_1,
     return table;
 }
 
-auto CallsTEvaluator::eval_calls_t(const QuotedIdent &quoted_proc_1,
-                                   const QuotedIdent &quoted_proc_2) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const QuotedIdent& quoted_proc_1, const QuotedIdent& quoted_proc_2) const
+    -> OutputTable {
     const auto& caller_name_string = quoted_proc_1.get_value();
     const auto& callee_name_string = quoted_proc_2.get_value();
     const bool does_call = read_facade->has_calls_star_relation(caller_name_string, callee_name_string);
@@ -113,8 +105,7 @@ auto CallsTEvaluator::eval_calls_t(const QuotedIdent &quoted_proc_1,
     return UnitTable{};
 }
 
-auto CallsTEvaluator::eval_calls_t(const QuotedIdent &quoted_proc_1,
-                                   const WildCard &) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const QuotedIdent& quoted_proc_1, const WildCard&) const -> OutputTable {
     const bool is_caller_star = read_facade->contains_calls_star_key(quoted_proc_1.get_value());
 
     if (!is_caller_star) {
@@ -124,8 +115,8 @@ auto CallsTEvaluator::eval_calls_t(const QuotedIdent &quoted_proc_1,
     return UnitTable{};
 }
 
-auto CallsTEvaluator::eval_calls_t(const WildCard &,
-                                   const std::shared_ptr<ProcSynonym> &proc_syn_2) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const WildCard&, const std::shared_ptr<ProcSynonym>& proc_syn_2) const
+    -> OutputTable {
     auto table = Table{{proc_syn_2}};
     const auto all_callees = read_facade->get_all_calls_star_values();
     for (const auto& callee : all_callees) {
@@ -134,8 +125,7 @@ auto CallsTEvaluator::eval_calls_t(const WildCard &,
     return table;
 }
 
-auto CallsTEvaluator::eval_calls_t(const WildCard &,
-                                   const QuotedIdent &quoted_proc_2) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const WildCard&, const QuotedIdent& quoted_proc_2) const -> OutputTable {
     const bool is_callee_star = read_facade->contains_calls_star_value(quoted_proc_2.get_value());
 
     if (!is_callee_star) {
@@ -145,8 +135,7 @@ auto CallsTEvaluator::eval_calls_t(const WildCard &,
     return UnitTable{};
 }
 
-auto CallsTEvaluator::eval_calls_t(const WildCard &,
-                                   const WildCard &) const -> OutputTable {
+auto CallsTEvaluator::eval_calls_t(const WildCard&, const WildCard&) const -> OutputTable {
     const bool calls_star_exists = read_facade->has_calls_star_relation();
 
     if (!calls_star_exists) {
@@ -155,4 +144,4 @@ auto CallsTEvaluator::eval_calls_t(const WildCard &,
 
     return UnitTable{};
 }
-}
+} // namespace qps
