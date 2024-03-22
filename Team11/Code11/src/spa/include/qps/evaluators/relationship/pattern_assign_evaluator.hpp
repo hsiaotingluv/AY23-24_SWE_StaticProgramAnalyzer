57fde5431ed2555c625af7dd08c439eb4af7b0f4
timiditi <timothy.ng.jw@gmail.com>
2024-03-22 10:54:56 +0800

    refactor: update pattern assign function names

--- Team11/Code11/src/spa/include/qps/evaluators/relationship/pattern_assign_evaluator.hpp
+++ Team11/Code11/src/spa/include/qps/evaluators/relationship/pattern_assign_evaluator.hpp
@@ -14,35 +14,35 @@ class PatternAssignEvaluator : public ClauseEvaluator {
     [[nodiscard]] auto select_eval_method() const;
 
     // e.g. pattern a(x, _"v"_)
-    [[nodiscard]] auto eval_pattern(const std::shared_ptr<Synonym>& synonym,
-                                    const qps::PartialMatch& partial_match) const -> OutputTable;
+    [[nodiscard]] auto eval_pattern_assign(const std::shared_ptr<Synonym>& synonym,
+                                           const qps::PartialMatch &partial_match) const -> OutputTable;
 
     // e.g. pattern a(x, _)
-    [[nodiscard]] auto eval_pattern(const std::shared_ptr<Synonym>& synonym, const qps::WildCard&) const -> OutputTable;
+    [[nodiscard]] auto eval_pattern_assign(const std::shared_ptr<Synonym>& synonym, const qps::WildCard &) const -> OutputTable;
 
     // e.g. pattern a("x", _"v"_)
-    [[nodiscard]] auto eval_pattern(const qps::QuotedIdent& quoted_ident, const qps::PartialMatch& partial_match) const
+    [[nodiscard]] auto eval_pattern_assign(const qps::QuotedIdent &quoted_ident, const qps::PartialMatch &partial_match) const
         -> OutputTable;
 
     // e.g. pattern a("x", _)
-    [[nodiscard]] auto eval_pattern(const qps::QuotedIdent& quoted_ident, const qps::WildCard&) const -> OutputTable;
+    [[nodiscard]] auto eval_pattern_assign(const qps::QuotedIdent &quoted_ident, const qps::WildCard &) const -> OutputTable;
 
     // e.g. pattern a(_, _"v"_)
-    [[nodiscard]] auto eval_pattern(const qps::WildCard& wild_card, const qps::PartialMatch& partial_match) const
+    [[nodiscard]] auto eval_pattern_assign(const qps::WildCard &wild_card, const qps::PartialMatch &partial_match) const
         -> OutputTable;
 
     // e.g. pattern a(_, _)
-    [[nodiscard]] auto eval_pattern(const qps::WildCard&, const qps::WildCard&) const -> OutputTable;
+    [[nodiscard]] auto eval_pattern_assign(const qps::WildCard &, const qps::WildCard &) const -> OutputTable;
 
     // e.g. pattern a(_, "v")
-    [[nodiscard]] auto eval_pattern(const WildCard&, const ExactMatch& exact) const -> OutputTable;
+    [[nodiscard]] auto eval_pattern_assign(const WildCard&, const ExactMatch& exact) const -> OutputTable;
 
     // e.g. pattern a(x, "v")
-    [[nodiscard]] auto eval_pattern(const std::shared_ptr<Synonym>& synonym, const ExactMatch& exact) const
+    [[nodiscard]] auto eval_pattern_assign(const std::shared_ptr<Synonym>& synonym, const ExactMatch& exact) const
         -> OutputTable;
 
     // e.g. pattern a("x", "v")
-    [[nodiscard]] auto eval_pattern(const QuotedIdent& quoted_ident, const ExactMatch& exact) const -> OutputTable;
+    [[nodiscard]] auto eval_pattern_assign(const QuotedIdent& quoted_ident, const ExactMatch& exact) const -> OutputTable;
 
   public:
     PatternAssignEvaluator(std::shared_ptr<pkb::ReadFacade> read_facade, PatternAssign pattern)
