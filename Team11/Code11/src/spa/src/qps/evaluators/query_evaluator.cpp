8869dc70b2e7ff3cce505610c21e640897c242f4
timiditi <timothy.ng.jw@gmail.com>
2024-04-09 11:54:30 +0800

    chore: style formatting

--- Team11/Code11/src/spa/src/qps/evaluators/query_evaluator.cpp
+++ Team11/Code11/src/spa/src/qps/evaluators/query_evaluator.cpp
@@ -31,12 +31,10 @@ auto QueryEvaluator::evaluate_query(const Query& query_obj) -> OutputTable {
         bool is_negated = is_first_clause && clause->is_negated_clause();
         if (const auto such_that_clause = std::dynamic_pointer_cast<qps::SuchThatClause>(clause)) {
             const auto relationship = such_that_clause->rel_ref;
-            evaluator =
-                std::visit(such_that_clause_evaluator_selector(read_facade, is_negated), relationship);
+            evaluator = std::visit(such_that_clause_evaluator_selector(read_facade, is_negated), relationship);
         } else if (const auto pattern_clause = std::dynamic_pointer_cast<qps::PatternClause>(clause)) {
             const auto syntactic_pattern = pattern_clause->syntactic_pattern;
-            evaluator = std::visit(pattern_clause_evaluator_selector(read_facade, is_negated),
-                                   syntactic_pattern);
+            evaluator = std::visit(pattern_clause_evaluator_selector(read_facade, is_negated),syntactic_pattern);
         } else if (const auto with_clause = std::dynamic_pointer_cast<qps::WithClause>(clause)) {
             evaluator = std::make_shared<WithEvaluator>(read_facade, with_clause->ref1, with_clause->ref2,
                                                         with_clause->is_negated_clause());
